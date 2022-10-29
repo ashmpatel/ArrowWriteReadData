d075616b6e92a4730e3514024a827341ab97e070
Ashish Patel <ashmpatel@gmail.com>
2022-10-29 13:06:21 +0100

    crete arrow file, write data, read data, read only the column we want from the arrow file

--- src/main/java/org/example/ReadWriteArrow.java
+++ src/main/java/org/example/ReadWriteArrow.java
@@ -1,6 +1,13 @@
 package org.example;
 
 
+import org.apache.arrow.dataset.file.FileFormat;
+import org.apache.arrow.dataset.file.FileSystemDatasetFactory;
+import org.apache.arrow.dataset.jni.NativeMemoryPool;
+import org.apache.arrow.dataset.scanner.ScanOptions;
+import org.apache.arrow.dataset.scanner.Scanner;
+import org.apache.arrow.dataset.source.Dataset;
+import org.apache.arrow.dataset.source.DatasetFactory;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.IntVector;
@@ -8,6 +15,7 @@ import org.apache.arrow.vector.VarCharVector;
 import org.apache.arrow.vector.VectorSchemaRoot;
 import org.apache.arrow.vector.ipc.ArrowFileReader;
 import org.apache.arrow.vector.ipc.ArrowFileWriter;
+import org.apache.arrow.vector.ipc.ArrowReader;
 import org.apache.arrow.vector.ipc.message.ArrowBlock;
 import org.apache.arrow.vector.types.pojo.ArrowType;
 import org.apache.arrow.vector.types.pojo.Field;
@@ -18,61 +26,71 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Optional;
 
 import static java.util.Arrays.asList;
 
 public class ReadWriteArrow {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            System.out.println("Started");
+        System.out.println("Started");
 
-            writeArrowFile();
-            readArrowFile();
+        writeArrowFile();
+        readArrowFile();
+        projectionArrowRead();
+        queryArrowData();
 
-            System.out.println("Completed");
-        }
+        System.out.println("Completed");
+    }
 
 
-        public  static void writeArrowFile() {
-            try (BufferAllocator allocator = new RootAllocator()) {
-                Field name = new Field("name", FieldType.nullable(new ArrowType.Utf8()), null);
-                Field age = new Field("age", FieldType.nullable(new ArrowType.Int(32, true)), null);
-                Schema schemaPerson = new Schema(asList(name, age));
+    /**
+     * Create an arrow file and write the data to it
+     */
+    public static void writeArrowFile() {
+        try (BufferAllocator allocator = new RootAllocator()) {
+            Field name = new Field("name", FieldType.nullable(new ArrowType.Utf8()), null);
+            Field age = new Field("age", FieldType.nullable(new ArrowType.Int(32, true)), null);
+            Schema schemaPerson = new Schema(asList(name, age));
+            try (
+                    VectorSchemaRoot vectorSchemaRoot = VectorSchemaRoot.create(schemaPerson, allocator)
+            ) {
+                VarCharVector nameVector = (VarCharVector) vectorSchemaRoot.getVector("name");
+                nameVector.allocateNew(3);
+                nameVector.set(0, "David".getBytes());
+                nameVector.set(1, "Gladis".getBytes());
+                nameVector.set(2, "Juan".getBytes());
+
+                IntVector ageVector = (IntVector) vectorSchemaRoot.getVector("age");
+                ageVector.allocateNew(3);
+                ageVector.set(0, 10);
+                ageVector.set(1, 20);
+                ageVector.set(2, 30);
+                vectorSchemaRoot.setRowCount(3);
+
+                File file = new File("random_access_to_file.arrow");
                 try (
-                        VectorSchemaRoot vectorSchemaRoot = VectorSchemaRoot.create(schemaPerson, allocator)
+                        FileOutputStream fileOutputStream = new FileOutputStream(file);
+                        ArrowFileWriter writer = new ArrowFileWriter(vectorSchemaRoot, null, fileOutputStream.getChannel())
                 ) {
-                    VarCharVector nameVector = (VarCharVector) vectorSchemaRoot.getVector("name");
-                    nameVector.allocateNew(3);
-                    nameVector.set(0, "David".getBytes());
-                    nameVector.set(1, "Gladis".getBytes());
-                    nameVector.set(2, "Juan".getBytes());
-
-                    IntVector ageVector = (IntVector) vectorSchemaRoot.getVector("age");
-                    ageVector.allocateNew(3);
-                    ageVector.set(0, 10);
-                    ageVector.set(1, 20);
-                    ageVector.set(2, 30);
-                    vectorSchemaRoot.setRowCount(3);
-
-                    File file = new File("random_access_to_file.arrow");
-                    try (
-                            FileOutputStream fileOutputStream = new FileOutputStream(file);
-                            ArrowFileWriter writer = new ArrowFileWriter(vectorSchemaRoot, null, fileOutputStream.getChannel())
-                    ) {
-                        writer.start();
-                        writer.writeBatch();
-                        writer.end();
-                        System.out.println("Record batches written: " + writer.getRecordBlocks().size() + ". Number of rows written: " + vectorSchemaRoot.getRowCount());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    writer.start();
+                    writer.writeBatch();
+                    writer.end();
+                    System.out.println("Record batches written: " + writer.getRecordBlocks().size() + ". Number of rows written: " + vectorSchemaRoot.getRowCount());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         }
+    }
 
+    /**
+     * Read the data in the arrow file and display it
+     */
     public static void readArrowFile() {
         File file = new File("random_access_to_file.arrow");
+
         try (
                 BufferAllocator rootAllocator = new RootAllocator();
                 FileInputStream fileInputStream = new FileInputStream(file);
@@ -85,6 +103,54 @@ public class ReadWriteArrow {
                 System.out.print(vectorSchemaRootRecover.contentToTSVString());
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Some metadata about the stuff we stored
+     */
+    public static void queryArrowData() {
+        String uri = "file:///c:/code/arrowreadwrite/random_access_to_file.arrow";
+        ScanOptions options = new ScanOptions(/*batchSize*/ 32768);
+        try (
+                BufferAllocator allocator = new RootAllocator();
+                DatasetFactory datasetFactory = new FileSystemDatasetFactory(allocator, NativeMemoryPool.getDefault(), FileFormat.ARROW_IPC, uri);
+                Dataset dataset = datasetFactory.finish();
+                Scanner scanner = dataset.newScan(options);
+                ArrowReader reader = scanner.scanBatches()
+        ) {
+            int count = 1;
+            while (reader.loadNextBatch()) {
+                try (VectorSchemaRoot root = reader.getVectorSchemaRoot()) {
+                    System.out.println("Number of rows per batch[" + count++ + "]: " + root.getRowCount());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Read only the columns we want
+     */
+    public static void projectionArrowRead() {
+        String uri = "file:///c:/code/arrowreadwrite/random_access_to_file.arrow";
+        String[] projection = new String[]{"name"};
+        ScanOptions options = new ScanOptions(/*batchSize*/ 32768, Optional.of(projection));
+        try (
+                BufferAllocator allocator = new RootAllocator();
+                DatasetFactory datasetFactory = new FileSystemDatasetFactory(allocator, NativeMemoryPool.getDefault(), FileFormat.ARROW_IPC, uri);
+                Dataset dataset = datasetFactory.finish();
+                Scanner scanner = dataset.newScan(options);
+                ArrowReader reader = scanner.scanBatches()
+        ) {
+            while (reader.loadNextBatch()) {
+                try (VectorSchemaRoot root = reader.getVectorSchemaRoot()) {
+                    System.out.print(root.contentToTSVString());
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
